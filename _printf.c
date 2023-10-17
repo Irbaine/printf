833dd095daed2ac826debd3d26e7709c1aceeaf9
Irbaine <irbaine@yahoo.com>
2023-10-16 19:52:25 -0700

    Betty Fixes

--- _printf.c
+++ _printf.c
@@ -3,14 +3,17 @@
  * _printf - Custom printf function
  * @format: The format string with placeholders
  * @...: Variable number of arguments
- * Returns: The number of characters printed
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	new m[] = {
-		{"%c", printf_char}, {"%s", print_string}, {"%%", print_37}, {"%d", print_dec}, {"%i", print_int},
-		{"%r", print_reve}, {"%R", print_rot13}, {"%b", print_bin}, {"%u", print_unsigned}, {"%o", print_oct},
-		{"%x", print_hex}, {"%X", print_HEX}, {"%S", print_string}, {"%p", print_pointer}
+		{"%c", printf_char}, {"%s", print_string}, {"%%", print_37},
+		{"%d", print_dec}, {"%i", print_int},
+		{"%r", print_reve}, {"%R", print_rot13}, {"%b", print_bin},
+		{"%u", print_unsigned}, {"%o", print_oct},
+		{"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_string}, {"%p", print_pointer}
 	};
 
 	va_list args;
@@ -22,20 +25,20 @@ int _printf(const char *format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 Here:
-	while (format [i] != '\0')
+	while (format[i] != '\0')
 	{
 		j = 13;
-		while (j>= 0)
+		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
 				len = len + m[j].f(args);
-				i= i + 2;
+				i = i + 2;
 				goto Here;
 			}
 			j--;
 		}
-		_putchar(format [i]);
+		_putchar(format[i]);
 		i++;
 		len++;
 	}
