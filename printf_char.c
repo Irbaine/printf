7e3d7a808a054451039a5ff525e4dafd4ba837af
Irbaine <irbaine@yahoo.com>
2023-10-17 04:22:33 -0700

    Betty Fix

--- printf_char.c
+++ printf_char.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-* printf_char - prints char.
-* @val : an argument .
+* printf_char - prints char
+* @val : an argument
 * Return: 1.
  */
+
 int printf_char(va_list val)
 {
-        char c ;
+	char c;
 
-        c = va_arg(val,int);
-        _putchar(c);
-        return(1);
+	c = va_arg(val, int);
+	_putchar(c);
+	return (1);
 }
