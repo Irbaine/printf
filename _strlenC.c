b70dada0a2ce328fa25042c7c54b63afa3070b45
Mounim <mounimnadir7@gmail.com>
2023-10-17 13:20:56 -0700

    updated

--- _strlenC.c
+++ _strlenC.c
@@ -6,22 +6,30 @@
 */
 
 int _strlen(char *str)
+
 {
 	int i;
 
-	for (i = 0 ; str[i] != 0; i++);
+	for (i = 0 ; str[i] != 0; i++)
+	{
+
+	}
 	return (i);
 }
 /**
- * _strlenC- Same as _strlen applied for constant char pointer s.
+ * _strlenC - Same as _strlen applied for constant char pointer s.
  * @str: char pointer
  * Return: the length of the const string
- */
+*/
 
 int _strlenC(const char *str)
+
 {
 	int i;
 
-	for (i = 0 ; str[i] != 0 ; i++);
+	for (i = 0 ; str[i] != 0 ; i++)
+	{
+
+	}
 	return (i);
 }
