f3a0a6022de21e8e89157b3db2fb7b1a18848e76
Irbaine <irbaine@yahoo.com>
2023-10-17 02:23:08 -0700

    Betty fix

--- flags.c
+++ flags.c
@@ -10,9 +10,9 @@ int print_flags(const char *format, int *i)
 {
 	int j, a;
 	int flags = 0;
-  const int FLAGS_ARRAY[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-
+	const int FLAGS_ARRAY[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+
 	for (a = *i + 1; format[a] != '\0'; a++)
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
